d9913a1f3828a7bff363049c6dba5e2cbccd493b
OFCode-Dev <ofaruk1991@gmail.com>
2026-05-05 08:58:05 +0000

    feat(theme): add darkTheme variant

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -12,3 +12,9 @@ final ThemeData appTheme = ThemeData(
     elevation: 0,
   ),
 );
+
+final darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark),
+  appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+);
