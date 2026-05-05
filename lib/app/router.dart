ce854a84e79edefa3d0650d2f45f8ee0d52bfa7a
OFCode-Dev <ofaruk1991@gmail.com>
2026-05-05 08:55:13 +0000

    feat(router): add /onboarding and categoryId path params

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,28 +1,23 @@
 import 'package:go_router/go_router.dart';
-
 import '../features/home/home_screen.dart';
 import '../features/quiz/quiz_screen.dart';
 import '../features/result/result_screen.dart';
 import '../features/settings/settings_screen.dart';
+import '../features/onboarding/onboarding_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
+    GoRoute(path: '/', builder: (c, s) => const HomeScreen()),
+    GoRoute(path: '/onboarding', builder: (c, s) => const OnboardingScreen()),
     GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
+      path: '/quiz/:categoryId',
+      builder: (c, s) => QuizScreen(categoryId: s.pathParameters['categoryId']!),
     ),
     GoRoute(
-      path: '/quiz',
-      builder: (context, state) => const QuizScreen(),
+      path: '/result/:categoryId',
+      builder: (c, s) => ResultScreen(categoryId: s.pathParameters['categoryId']!),
     ),
-    GoRoute(
-      path: '/result',
-      builder: (context, state) => const ResultScreen(),
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (context, state) => const SettingsScreen(),
-    ),
+    GoRoute(path: '/settings', builder: (c, s) => const SettingsScreen()),
   ],
 );
